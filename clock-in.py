3149fd5708822e7e5bc95d982589d64318184b7c
MChuanHuan <88417721+MChuanHuan@users.noreply.github.com>
2022-05-07 19:06:57 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -26,6 +26,7 @@ class DaKa(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.imgaddress = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
